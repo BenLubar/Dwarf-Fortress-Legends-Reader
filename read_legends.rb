e3cb71ee90ccdd789acbc2ac1bea536b3e779ff4
Ben Lubar <ben.lubar@gmail.com>
2014-01-04 13:22:08 -0600

    began wandering

--- read_legends.rb
+++ read_legends.rb
@@ -74,11 +74,11 @@ def write_figure data
         line_accum.gsub! /,\s+(the\s+[^A-Z]+)([A-Z].*?)\s+(struck\s+down|shot\s+and\s+killed|attacked|was\s+struck\s+down\s+by|was\s+shot\s+and\s+killed\s+by|devoured|ambushed|fought\s+with|happened\s+upon|confronted)\s+#{first_name}\.(\s+While\s+defeated,\s+the\s+latter\s+escaped\s+unscathed\.)?\z/ do
           ", #{$1} <a href=\"fig-#{$2.paramcase}.html\">#{$2}</a> #{$3} #{first_name}.#{$4}"
         end
-        line_accum.gsub! /\b#{first_name}\s+became\s+(an\s+enemy|the\s+.*?)\s+of\s+([A-Z].*)\.\z/ do
-          "#{first_name} became #{$1} of <a href=\"ent-#{$2.paramcase}.html\">#{$2}</a>."
+        line_accum.gsub! /,\s+#{first_name}\s+became\s+(an\s+enemy|the\s+.*?)\s+of\s+([A-Z].*)\.\z/ do
+          ", #{first_name} became #{$1} of <a href=\"ent-#{$2.paramcase}.html\">#{$2}</a>."
         end
-        line_accum.gsub! /\b#{first_name}\s+(settled\s+in)\s+([A-Z].*)\.\z/ do
-          "#{first_name} #{$1} <a href=\"site-#{$2.paramcase}.html\">#{$2}</a>."
+        line_accum.gsub! /,\s+#{first_name}\s+(settled\s+in|began\s+wandering)\s+([A-Z][^\.]*?)\.\z/ do
+          ", #{first_name} #{$1} <a href=\"site-#{$2.paramcase}.html\">#{$2}</a>."
         end
       else
         line_accum.gsub! /\A(.*?)\s+was\s+a\b/ do

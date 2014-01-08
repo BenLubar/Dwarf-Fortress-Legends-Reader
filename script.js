5bf6301c4fa1abe3243339aea58a983756a2857a
Ben Lubar <ben.lubar@gmail.com>
2014-01-08 13:03:34 -0600

    fix javascript

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-Array.prototype.forEach.call(document.querySelectorAll('a[href]'), function(a) {
-	a.addEventListener("mouseover", function() {
-		Array.prototype.forEach.call(document.querySelectorAll('a[href="' + a.getAttribute('href') + '"]'), function(a) {
-			a.className = 'hover';
-		});
-	}, false);
-	a.addEventListener("mouseout", function() {
-		Array.prototype.forEach.call(document.querySelectorAll('a[href="' + a.getAttribute('href') + '"]'), function(a) {
-			a.className = '';
-		});
-	}, false);
-});
+(function(f) {
+	if (document.readyState == 'loading')
+		document.addEventListener('DOMContentLoaded', f, false);
+	else
+		f();
+})(function() {
+	Array.prototype.forEach.call(document.querySelectorAll('a[href]'), function(a) {
+		a.addEventListener("mouseover", function() {
+			Array.prototype.forEach.call(document.querySelectorAll('a[href="' + a.getAttribute('href') + '"]'), function(a) {
+				a.className = 'hover';
+			});
+		}, false);
+		a.addEventListener("mouseout", function() {
+			Array.prototype.forEach.call(document.querySelectorAll('a[href="' + a.getAttribute('href') + '"]'), function(a) {
+				a.className = '';
+			});
+		}, false);
+	});
+})
